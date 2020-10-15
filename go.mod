0618726baf0cf26a283731e0db1aa3cde306ff77
Artur Propp <artur.propp@me.com>
2020-10-15 20:39:09 +0200

    feat: impl basic feature set

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/artpropp/goconc
+
+go 1.15
---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseArgs(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []string
+		want  []cmdArgs
+	}{
+		{
+			"two commands",
+			[]string{"abc", "def", "::", "abcd", "efgh"},
+			[]cmdArgs{
+				{"abc", []string{"def"}},
+				{"abcd", []string{"efgh"}},
+			},
+		},
+		{
+			"single command",
+			[]string{"abc", "def", "ghi"},
+			[]cmdArgs{
+				{"abc", []string{"def", "ghi"}},
+			},
+		},
+		{
+			"no command",
+			[]string{},
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := parseArgs(tt.input)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseArgs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
